20de45ffa781ab6f5b051ef7ba2e131168990501
root <root@ip-172-31-46-160.ap-south-1.compute.internal>
2024-01-31 05:41:17 +0000

    new

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiGatewayURL: 'http://hadiya-load-balancer-1094884867.ap-south-1.elb.amazonaws.com:3000'
+  apiGatewayURL: 'hadiya-load-balancer-766016301.ap-south-1.elb.amazonaws.com:3000'
 };
